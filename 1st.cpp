c359e75ccc78540f6973b7a73dd174d2bf4e3517
amitgrinds <amitcoder01@gmail.com>
2026-08-31 00:11:34 +0530

    Initial commit

---
+++ 1st.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    #include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int x = 3;    
+    double result = pow(x, 2) + sqrt(16);
+    cout << result;    
+    return 0;
+}
+    return 0;
+}
